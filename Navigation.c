3c588907fe96f1cbdc57062ae708fbd360e91262
JohnKalesis1 <61728881+JohnKalesis1@users.noreply.github.com>
2020-03-05 18:29:02 +0200

    Update Navigation.c

--- Navigation.c
+++ Navigation.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>  
+/*
+Directions:
+  Take the <NEXT,2nd,3rd,4th,5th> <LEFT,RIGHT>
+  Go straight on for <100,200,300,...,900>m
+  Go straight on for <1.0,1.1,1.2,...,5.0>km
+  Turn around!
+NOTES:
+  First and last messages are always present
+  There may be any number of en-route messages
+  Messages are always formatted corrrectly
+  A NEXT turn message means you must to proceed to the next cross-street even if you are currently at an intersection
+  But Turn around does not need to be done at an intersection. Just do a U-turn wherever you are
+*/
 int main()  {
     int x,y,movement_axis,turn;
     char trash[10],text_data[10],nav_data[10];
